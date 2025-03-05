56740a51cae71eb4fe0bafd0a9bb848514c2fb8f
hires7 <hires7@stud.uniza.sk>
2025-03-05 19:11:02 +0100

    opravena inicializacia databazy a vytvaranie admina

--- Bakalarska_praca/App.xaml.cs
+++ Bakalarska_praca/App.xaml.cs
@@ -9,5 +9,7 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+        DatabaseInitializer.Initialize();
+        DatabaseInitializer.CreateAdminUser();
     }
 }
