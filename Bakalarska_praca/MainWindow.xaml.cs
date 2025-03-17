11ebd70406e78261e6f34d8d17bbd2e240618297
hires7 <hires7@stud.uniza.sk>
2025-03-17 23:28:41 +0100

    vyzadovanie prihlasenia pre manipulaciu s ciselnikmi

--- Bakalarska_praca/MainWindow.xaml.cs
+++ Bakalarska_praca/MainWindow.xaml.cs
@@ -1,82 +1,55 @@
 ﻿using Bakalarska_praca.Core.Services;
 using Bakalarska_praca.UI.ViewModels;
 using Bakalarska_praca.UI.Views;
-using System.Text;
+using System;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Bakalarska_praca;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace Bakalarska_praca
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-    }
+        private readonly MainViewModel _viewModel;
 
-    private void Login_Click(object sender, RoutedEventArgs e)
-    {
-        LoginView loginWindow = new LoginView();
-        loginWindow.ShowDialog();
-    }
-
-    private void Logout_Click(object sender, RoutedEventArgs e)
-    {
-        UserService.Logout();
-        Console.WriteLine(" Používateľ odhlásený.");
-
-        if (Application.Current.Windows.OfType<LoginView>().FirstOrDefault() is LoginView loginWindow)
+        public MainWindow()
         {
-            loginWindow.Show();
-        }
-        else
-        {
-            loginWindow = new LoginView();
-            loginWindow.Show();
+            InitializeComponent();
+            _viewModel = new MainViewModel();
+            DataContext = _viewModel;
         }
 
-        Application.Current.MainWindow.Hide();
-
-        if (loginWindow.DataContext is LoginViewModel loginViewModel)
+        private void Login_Click(object sender, RoutedEventArgs e)
         {
-            loginViewModel.ShowError = false;
+            LoginView loginWindow = new LoginView();
+            loginWindow.ShowDialog();
+
+            _viewModel.RefreshUser();
         }
 
-        if (loginWindow.DataContext is MainViewModel mainViewModel)
+        private void Logout_Click(object sender, RoutedEventArgs e)
         {
-            mainViewModel.RefreshUser();
+            UserService.Logout();
+            Console.WriteLine("Používateľ odhlásený.");
+
+            _viewModel.RefreshUser();
+        }
+
+        private void ChangePassword_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePasswordView changePasswordView = new ChangePasswordView();
+            changePasswordView.ShowDialog();
+        }
+
+        private void Users_Click(object sender, RoutedEventArgs e)
+        {
+            UsersView usersView = new UsersView();
+            usersView.ShowDialog();
+        }
+
+        private void OpenMaterialsView(object sender, RoutedEventArgs e)
+        {
+            MaterialsView materialsView = new MaterialsView();
+            materialsView.ShowDialog();
         }
     }
-
-    private void ChangePassword_Click(object sender, RoutedEventArgs e)
-    {
-        ChangePasswordView changePasswordView = new ChangePasswordView();
-        changePasswordView.ShowDialog();
-    }
-
-    private void Users_Click(object sender, RoutedEventArgs e)
-    {
-        UsersView usersView = new UsersView();
-        usersView.ShowDialog();
-    }
-
-    private void OpenMaterialsView(object sender, RoutedEventArgs e)
-    {
-        MaterialsView materialsView = new MaterialsView();
-        materialsView.ShowDialog();
-    }
-
-
 }
-
-
